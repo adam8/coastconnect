3a7a82b0665cc53c8f72e298da24e3154acc0b56
Adam Jones <adam@adam8.com>
2015-05-25 09:30:18 -0700

    fix rabbit hole5

--- api.js
+++ api.js
@@ -29,5 +29,5 @@ var server = app.listen(3000, function () {
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log('App is listening on http://%s:%s', host, port);
+    console.log('App is listening over here http://%s:%s', host, port);
 });
