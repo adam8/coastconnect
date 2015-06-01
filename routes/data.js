fac792c6e9388fafa236bc1ba9e9ddd8a774af44
Adam Jones <adam@adam8.com>
2015-06-01 10:49:20 -0700

    /data post json

--- routes/data.js
+++ routes/data.js
@@ -4,20 +4,27 @@ var auth = require('../lib/auth');
 var router = express.Router();
 
 router.post('/', auth.authorize, function (request, response) {
+  
+  var events = request.body.events;
+  var newEvents = [];
+  
+  for (var i = 0; i < events.length; i++) {
+    var newEvent = {
+      time:events[i].time,
+      date: new Date(events[i].time * 1000),
+      category:events[i].category,
+      priority:events[i].priority,
+      deadline:events[i].deadline,
+      activity:events[i].activity,
+      detail:events[i].detail,
+      link:events[i].link
+    };
+    newEvents.push(newEvent);
+  }
 
-  var newEvent = {
-    time:1431742298.993,
-    category:"maintenance",
-    priority:null,
-    deadline:null,
-    activity:"new activity, it worked!",
-    detail:"new detail here",
-    link:null
-  };
+  console.log(newEvents);
 
-  console.log(newEvent);
-
-  rdb.save('events', newEvent)
+  rdb.save('events', newEvents)
   .then(function (result) {
       response.json(result);
   });
