0120b5f309a0561334982d577efcb22f7c50488d
Adam Jones <adam@adam8.com>
2015-06-01 11:00:08 -0700

    /data post json3

--- routes/data.js
+++ routes/data.js
@@ -10,14 +10,14 @@ router.post('/', auth.authorize, function (request, response) {
   
   for (var i = 0; i < events.length; i++) {
     var newEvent = {
-      time:events[i].time,
-      date: new Date(events[i].time * 1000),
-      category:events[i].category,
-      priority:events[i].priority,
-      deadline:events[i].deadline,
-      activity:events[i].activity,
-      detail:events[i].detail,
-      link:events[i].link
+      time:events[i].time// ,
+//       date: new Date(events[i].time * 1000),
+//       category:events[i].category,
+//       priority:events[i].priority,
+//       deadline:events[i].deadline,
+//       activity:events[i].activity,
+//       detail:events[i].detail,
+//       link:events[i].link
     };
     newEvents.push(newEvent);
   }
