e7581f8bde6bcb90352940f6308cc7f70171292d
Adam Jones <adam@adam8.com>
2015-06-02 14:15:01 -0700

    log request body4

--- routes/login.js
+++ routes/login.js
@@ -11,27 +11,29 @@ router.post('/', function (request, response, next) {
         user = user[0];
 
         if(!user) {
-            var userNotFoundError = new Error('User not found');
-            userNotFoundError.status = 404;
-            return next(userNotFoundError);
+          var userNotFoundError = new Error('User not found');
+          userNotFoundError.status = 404;
+          return next(userNotFoundError);
         }
 
         auth.authenticate(request.body.password, user.password)
         .then(function (authenticated) {
             if(authenticated) {
-                var currentUser = {
-                    user_id: user.id,
-                    name: user.name,
-                    email: user.email,
-                    token: token.generate(user)
-                };
-                response.json(currentUser);
+              var currentUser = {
+                user_id: user.id,
+                name: user.name,
+                email: user.email,
+                streams: user.streams,
+                token: token.generate(user)
+              };
+              response.json(currentUser);
             } else {
-                var authenticationFailedError = new Error('Authentication failed');
-                authenticationFailedError.status = 401;
-                return next(authenticationFailedError);
+              var authenticationFailedError = new Error('Authentication failed');
+              authenticationFailedError.status = 401;
+              return next(authenticationFailedError);
             }
         });
+        
     });
 });
 
