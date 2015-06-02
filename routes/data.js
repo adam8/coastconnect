e7157365f23df57e4303a09ffa05dcbadb474de7
Adam Jones <adam@adam8.com>
2015-06-02 09:03:33 -0700

    cookie exp

--- routes/data.js
+++ routes/data.js
@@ -5,32 +5,30 @@ var router = express.Router();
 
 router.post('/', auth.authorize, function (request, response) {
   
-  var events = request.body;
-  var newEvents = [];
+  var ip = req.headers['x-forwarded-for'];
+  // console.log('forwarded-for', request.headers['x-forwarded-for']);
   
-  for (var i = 0; i < events.length; i++) {
-    var newEvent = {
-      time:events[i].time,
-      date: new Date(events[i].time),
-      activity:events[i].activity,
-      detail:events[i].detail,
-      category:events[i].category,
-      priority:events[i].priority,
-      deadline:events[i].deadline,
-      lat: events[i].lat,
-      long: events[i].long,
-      link:events[i].link
-    };
-    newEvents.push(newEvent);
-  }
-  
-  console.log('request.headers:', request.headers);
-  console.log('forwarded-for', request.headers['x-forwarded-for']);
-  console.log('remoteAddress:', request.connection.remoteAddress);
-  
-  // console.log('events.length',events.length);
-  // console.log('events',events);
-  // console.log('newEvents',newEvents);
+  // if (ip !== '24.207.111.128') {
+  //  response.json(result);
+  // } else {
+    var events = request.body;
+    var newEvents = [];
+    for (var i = 0; i < events.length; i++) {
+      var newEvent = {
+        time:events[i].time,
+        date: new Date(events[i].time),
+        activity:events[i].activity,
+        detail:events[i].detail,
+        category:events[i].category,
+        priority:events[i].priority,
+        deadline:events[i].deadline,
+        lat: events[i].lat,
+        long: events[i].long,
+        link:events[i].link
+      };
+      newEvents.push(newEvent);
+    }
+  // }
 
   rdb.save('events', newEvents)
   .then(function (result) {
