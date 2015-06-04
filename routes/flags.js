7075aef04ddf512711fd4a6e08d3e7aa16533b65
Adam Jones <adam@adam8.com>
2015-06-04 13:03:52 -0700

    code is hard

--- routes/flags.js
+++ routes/flags.js
@@ -13,25 +13,18 @@ router.get('/:id', auth.authorize, function (request, response, next) {
     }
     console.log("found user: " + user.name);
     console.log('flagged... ' + user.flagged);
-    //r.table('events').getAll(r.args(flags)).run(conn, callback)
-
-    // rdb.findArray('events', user.flagged)
-    // .then(function (flags) {
-    //   if(!flags) {
-    //     console.log('no flag results');
-    //     var notFoundError = new Error('No flags found');
-    //     notFoundError.status = 404;
-    //     return next(notFoundError);
-    //   }
-    //   console.log('flag result',findArray);
-    //   response.json(flags);
-    // });
-    
-    // response.json(rdb.table('events').getAll(r.args([ '46dfb2f0-89e7-461b-82a5-e0dbc2a97cd3',
-    // '8bd2202a-7854-4c9f-b2ff-12b9513c883e' ])).run(connection));
-    
-    response.json({'hola':'hey'});
-    
+    //r.table('events').getAll(r.args(flags)).run(conn, callback) 
+  });
+  rdb.findArray('events', user.flagged)
+  .then(function (events) {
+    if(!events) {
+      console.log('no flag event results');
+      var notFoundError = new Error('No flags found');
+      notFoundError.status = 404;
+      return next(notFoundError);
+    }
+    console.log('flag events result',findArray);
+    response.json(events);
   });
 });
 
