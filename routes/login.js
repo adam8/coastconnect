051afb4f73cb852dcf339ae04c75d11eb5d6cdc7
Adam Jones <adam@adam8.com>
2015-06-22 21:56:05 -0230

    remove auth

--- routes/login.js
+++ routes/login.js
@@ -20,23 +20,23 @@ router.post('/', function (request, response, next) {
         // r.db('coasteasy').table('users').insert({"name":"Joe Blow", "email":"foo@bar.baz", "password":"hola"})
         // uncomment this next line (and comment out the rest of this function) to generate the token key, used as x-api-token header
 
-//      response.json({'token':token.generate(user)});
-        auth.authenticate(request.body.password, user.password)
-        .then(function (authenticated) {
-            if(authenticated) {
-              var currentUser = {
-                id: user.id,
-                name: user.name,
-                email: user.email,
-                token: token.generate(user)
-              };
-              response.json(currentUser);
-            } else {
-              var authenticationFailedError = new Error('Authentication failed');
-              authenticationFailedError.status = 401;
-              return next(authenticationFailedError);
-            }
-        });
+        response.json({'token':token.generate(user)});
+        // auth.authenticate(request.body.password, user.password)
+        // .then(function (authenticated) {
+        //     if(authenticated) {
+        //       var currentUser = {
+        //         id: user.id,
+        //         name: user.name,
+        //         email: user.email,
+        //         token: token.generate(user)
+        //       };
+        //       response.json(currentUser);
+        //     } else {
+        //       var authenticationFailedError = new Error('Authentication failed');
+        //       authenticationFailedError.status = 401;
+        //       return next(authenticationFailedError);
+        //     }
+        // });
         
     });
 });
